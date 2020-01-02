258c83c707bb04dc14ab7457460df96f6bb54779
MrBembi13 <vasylrudyk13@gmail.com>
2020-01-02 19:23:04 +0200

    commit

--- TransCompByTrain/src/main/java/Executor.java
+++ TransCompByTrain/src/main/java/Executor.java
@@ -2,8 +2,8 @@ import company.person.Customer;
 import util.iokeyboard.InputKeyboard;
 
 public class Executor {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
         Customer customer1 = new Customer();
         InputKeyboard inputKeyboard = new InputKeyboard();
@@ -12,9 +12,9 @@ public class Executor {
         customer1.setName(inputKeyboard.inputString());
         System.out.println("Name - " + customer1.getName());
 
-
         System.out.println("Enter your Surname:");
         customer1.setSurname(inputKeyboard.inputString());
         System.out.println("Surname - " + customer1.getSurname());
+
     }
 }
