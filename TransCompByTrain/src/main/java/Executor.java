92fe802b8f64755a997ba68110f722628582399a
MrBembi13 <vasylrudyk13@gmail.com>
2020-01-04 16:20:03 +0200

    new commit for properties

--- TransCompByTrain/src/main/java/Executor.java
+++ TransCompByTrain/src/main/java/Executor.java
@@ -1,20 +1,35 @@
 import company.person.Customer;
 import util.iokeyboard.InputKeyboard;
+import util.iokeyboard.WriteFile;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Executor {
 
     public static void main(String[] args) {
+        FileInputStream fis;
+        Properties properties = new Properties();
+        String fileName = "D:\\test_auto_course\\git\\Java-course\\TransCompByTrain\\src\\main\\resources\\customer.properties";
+        try {
+            fis = new FileInputStream("src/main/resources/customer.properties");
+            Customer customer1 = new Customer();
+            InputKeyboard inputKeyboard = new InputKeyboard();
+            properties.load(fis);
 
-        Customer customer1 = new Customer();
-        InputKeyboard inputKeyboard = new InputKeyboard();
+            System.out.println("Enter your Name:");
+            customer1.setName(inputKeyboard.inputString());
 
-        System.out.println("Enter your Name:");
-        customer1.setName(inputKeyboard.inputString());
-        System.out.println("Name - " + customer1.getName());
+            System.out.println("Enter your Surname:");
+            customer1.setSurname(inputKeyboard.inputString());
+            WriteFile.write(fileName, "name = " + customer1.getName() + "\nsurname = " + customer1.getSurname());
 
-        System.out.println("Enter your Surname:");
-        customer1.setSurname(inputKeyboard.inputString());
-        System.out.println("Surname - " + customer1.getSurname());
+        } catch (IOException e){
+            System.err.println("error");
+        }
 
+        properties.getProperty("name");
+        properties.getProperty("surname");
     }
 }
