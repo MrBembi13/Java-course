39cd1a2cd9d42d0bc2f3d25d7edd136c3e2f4db7
MrBembi13 <vasylrudyk13@gmail.com>
2020-01-08 19:03:45 +0200

    make properties class

--- TransCompByTrain/src/main/java/Executor.java
+++ TransCompByTrain/src/main/java/Executor.java
@@ -1,35 +1,23 @@
 import company.person.Customer;
 import util.iokeyboard.InputKeyboard;
 import util.iokeyboard.WriteFile;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
+import util.properties.DoProperties;
 
 public class Executor {
 
     public static void main(String[] args) {
-        FileInputStream fis;
-        Properties properties = new Properties();
         String fileName = "D:\\test_auto_course\\git\\Java-course\\TransCompByTrain\\src\\main\\resources\\customer.properties";
-        try {
-            fis = new FileInputStream("src/main/resources/customer.properties");
-            Customer customer1 = new Customer();
-            InputKeyboard inputKeyboard = new InputKeyboard();
-            properties.load(fis);
 
-            System.out.println("Enter your Name:");
-            customer1.setName(inputKeyboard.inputString());
+        Customer customer1 = new Customer();
+        InputKeyboard inputKeyboard = new InputKeyboard();
 
-            System.out.println("Enter your Surname:");
-            customer1.setSurname(inputKeyboard.inputString());
-            WriteFile.write(fileName, "name = " + customer1.getName() + "\nsurname = " + customer1.getSurname());
+        System.out.println("Enter your Name:");
+        customer1.setName(inputKeyboard.inputString());
 
-        } catch (IOException e){
-            System.err.println("error");
-        }
+        System.out.println("Enter your Surname:");
+        customer1.setSurname(inputKeyboard.inputString());
+        WriteFile.write(fileName, "name = " + customer1.getName() + "\nsurname = " + customer1.getSurname());
 
-        properties.getProperty("name");
-        properties.getProperty("surname");
+        DoProperties.showProperties("src/main/resources/customer.properties");
     }
 }
