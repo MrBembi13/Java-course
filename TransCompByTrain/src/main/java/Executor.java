83c4f4dcc4ce2b5cc364f0629bfdcc1c15289e7a
MrBembi13 <vasylrudyk13@gmail.com>
2020-01-23 21:20:57 +0200

    add configuration

--- TransCompByTrain/src/main/java/Executor.java
+++ TransCompByTrain/src/main/java/Executor.java
@@ -1,18 +1,31 @@
+import company.partnercompany.PartnerCompany;
 import company.person.Customer;
-import util.iokeyboard.InputKeyboard;
-import util.iokeyboard.WriteFile;
+import mappers.PartnerCompanyMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
 
 public class Executor {
 
     public static void main(String[] args) {
 
-        Customer customer1 = new Customer();
-        InputKeyboard inputKeyboard = new InputKeyboard();
+        SqlSessionFactory sqlSessionFactory;
+        PartnerCompanyMapper partnerCompanyMapper;
+        Reader reader = null;
 
-        System.out.println("Enter your Name:");
-        customer1.setName(inputKeyboard.inputString());
-
-        System.out.println("Enter your Surname:");
-        customer1.setSurname(inputKeyboard.inputString());
+        try {
+            reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            partnerCompanyMapper = sqlSessionFactory.openSession().getMapper(PartnerCompanyMapper.class);
+            List partnerCompanies = partnerCompanyMapper.getPartnerCompany();
+            PartnerCompany partnerCompany = partnerCompanyMapper.getPartnerCompanyByID(1);
+            System.out.println(partnerCompany);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
