c320378a00b8f2e9e61b1ef0e1655de166ad05ed
MrBembi13 <vasylrudyk13@gmail.com>
2020-01-28 01:26:41 +0200

    mybatis and json

--- TransCompByTrain/src/main/java/Executor.java
+++ TransCompByTrain/src/main/java/Executor.java
@@ -1,36 +1,19 @@
-import company.partnercompany.PartnerCompany;
 import company.person.Customer;
-import company.person.Person;
-import company.train.CustomerTrain;
-import company.train.LuggageTrain;
-import company.transportation.LuggageTransportation;
-import company.transportation.Transportation;
+import company.person.CustomerModel;
 import mappers.*;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import util.OpenSession;
 
-import java.io.IOException;
-import java.io.Reader;
+
 import java.util.List;
 
 public class Executor {
 
     public static void main(String[] args) {
 
-        SqlSessionFactory sqlSessionFactory;
-        LuggageTransportationMapper customerMapper;
-        Reader reader = null;
+        CustomerMapper customerMapper;
 
-        try {
-            reader = Resources.getResourceAsReader("mybatis-config.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-            customerMapper = sqlSessionFactory.openSession().getMapper(LuggageTransportationMapper.class);
-            List persons = customerMapper.getLuggageTransportationByIDLT(1);
-            LuggageTransportation person = customerMapper.getLuggageTransportationByID(1);
-            System.out.println(person);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+        customerMapper = OpenSession.getOpenSession().openSession().getMapper(CustomerMapper.class);
+        CustomerModel customerModel = customerMapper.getCustomerByID(1);
+        System.out.println(customerModel);
     }
 }
